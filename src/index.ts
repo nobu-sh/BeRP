d99a3bd4ba3c58aeedcc60013389a24a9bb91a1e
Nobu <nowobuwu@gmail.com>
2021-08-11 23:05:52 -0500

    remove useless dependency & remove useless setInterval that was being used to keep process alive

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import { BeRP } from "./berp"
 import { resolve } from 'path'
 import { overrideProcessConsole } from './utils'
 
-setInterval(() => { /**/ },30)
-
-process.stdin.resume()
 overrideProcessConsole(resolve(process.cwd(), 'logs'))
 
 new BeRP()
