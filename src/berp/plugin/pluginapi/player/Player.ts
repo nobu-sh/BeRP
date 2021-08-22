af8437c464b1b5c60e8d34f6730ec4f3eed81cb0
PMK744 <69542280+PMK744@users.noreply.github.com>
2021-08-22 17:46:56 -0500

    Added nickname to Player

--- src/berp/plugin/pluginapi/player/Player.ts
+++ src/berp/plugin/pluginapi/player/Player.ts
@@ -5,6 +5,7 @@ import { PlayerOptions } from 'src/types/berp'
 
 export class Player {
   private _name: string
+  private _nickname: string
   private _uuid: string
   private _entityID: bigint
   private _runtimeID: bigint
@@ -14,6 +15,7 @@ export class Player {
   private _pluginApi: PluginApi
   constructor(options: PlayerOptions, berp: BeRP, connection: ConnectionHandler, pluginApi: PluginApi) {
     this._name = options.name
+    this._nickname = options.name
     this._uuid = options.uuid
     this._entityID = options.entityID
     this._runtimeID = options.runtimeID
@@ -24,19 +26,29 @@ export class Player {
     this._pluginApi.getPlayerManager().addPlayer(this)
   }
   public getName(): string { return this._name }
+  public getNickname(): string { return this._nickname }
   public getUUID(): string { return this._uuid }
   public getEntityID(): bigint { return this._entityID }
   public getRuntimeID(): bigint { return this._runtimeID }
   public getDevice(): number { return this._device }
+  public getExecutionName(): string {
+    if (this._name != this._nickname) return this._nickname
+
+    return this._name
+  }
+  public setNickname(nickname: string): void {
+    this._nickname = nickname
+    // TODO: Sends event to gametest
+  }
   public sendMessage(message: string): void {
-    this._pluginApi.getCommandManager().executeCommand(`tellraw "${this._name}" {"rawtext":[{"text":"${message}"}]}`)
+    this._pluginApi.getCommandManager().executeCommand(`tellraw "${this.getExecutionName()}" {"rawtext":[{"text":"${message}"}]}`)
   }
   public executeCommand(command: string): void {
-    this._pluginApi.getCommandManager().executeCommand(`execute "${this._name}" ~ ~ ~ ${command}`)
+    this._pluginApi.getCommandManager().executeCommand(`execute "${this.getExecutionName()}" ~ ~ ~ ${command}`)
   }
   public async getTags(): Promise<string[]> {
     return new Promise((r) => {
-      this._pluginApi.getCommandManager().executeCommand(`tag "${this._name}" list`, (err, res) => {
+      this._pluginApi.getCommandManager().executeCommand(`tag "${this.getExecutionName()}" list`, (err, res) => {
         if (err) return console.log(err)
         if (!res.output[0].paramaters[1]) return
         const filter = [res.output[0].paramaters[0], res.output[0].paramaters[1]]
@@ -55,7 +67,7 @@ export class Player {
   }
   public async getScore(objective: string): Promise<number> {
     return new Promise((r) => {
-      this._pluginApi.getCommandManager().executeCommand(`scoreboard players test "${this._name}" ${objective} * *`, (err, res) => {
+      this._pluginApi.getCommandManager().executeCommand(`scoreboard players test "${this.getExecutionName()}" ${objective} * *`, (err, res) => {
         if (err) return console.log(err)
         if (res.output[0].paramaters[0] == this._name) return r(0)
 
