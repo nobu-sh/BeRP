13cac5b2794039702fb462a98ce336a6b40deefc
DJ (Salvo) <85457381+DJStompZone@users.noreply.github.com>
2022-04-19 15:12:49 -0700

    Update game version, BeRP version++

--- src/Constants.ts
+++ src/Constants.ts
@@ -2,9 +2,9 @@ import { MCHeaders } from "./types/berp"
 import { resolve } from 'path'
 
 export const ProtoDataPath = resolve(process.cwd(), 'data')
-export const CUR_VERSION = '1.18.10'
+export const CUR_VERSION = '1.18.30'
 export const CUR_VERSION_PROTOCOL = 486
-export const BeRP_VERSION = '1.0.0'
+export const BeRP_VERSION = '1.0.1'
 
 const MCRAPI = "https://pocket.realms.minecraft.net/"
 
