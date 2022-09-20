404627d8fa0b2ac081d9fae9e78dbfe1bd668a40
DJ (Salvo) <85457381+DJStompZone@users.noreply.github.com>
2022-09-20 11:54:12 -0700

    Bump protocol version to 554

--- src/Constants.ts
+++ src/Constants.ts
@@ -2,9 +2,9 @@ import { MCHeaders } from "./types/berp"
 import { resolve } from 'path'
 
 export const ProtoDataPath = resolve(process.cwd(), 'data')
-export const CUR_VERSION = '1.19.21'
-export const CUR_VERSION_PROTOCOL = 545
-export const BeRP_VERSION = '1.0.5'
+export const CUR_VERSION = '1.19.30'
+export const CUR_VERSION_PROTOCOL = 554
+export const BeRP_VERSION = '1.0.6'
 
 const MCRAPI = "https://pocket.realms.minecraft.net/"
 
